98e0795e4308d9c9bc26b78896422083480b9573
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-22 15:32:00 -0500

    some more templating for chapter 2

---
+++ ch02/source.R
@@ -0,0 +1,45 @@
+##############################################################################
+# Chapter 2 code
+##############################################################################
+
+## R code 2.3 - 2.5
+# define main function
+main <- function() {
+  # compute likelihood at each value in grid
+  likelihood <- dbinom(x = 6, size = 9, prob = p_grid)
+  
+  # compute product of likelihood and prior
+  unstd_posterior <- likelihood * prior
+  
+  # standardize the posterior, so it sums to 1
+  posterior <- unstd_posterior / sum(unstd_posterior)
+  
+  # plot
+  par(mfrow=c(1,3))
+  plot(p_grid, prior, type = "b")
+  plot(p_grid, likelihood, type = "b")
+  plot(p_grid, posterior, type = "b")
+  return(invisible())
+}
+
+n <- 5
+for (i in 1:n) {
+  
+  # define grid
+  p_grid <- seq(from = 0, to = 1, length.out = n)
+  
+  # define prior
+  prior <- rep(1, length(p_grid))
+  main()
+  
+  # define prior
+  prior <- ifelse(p_grid < 0.5, 0, 1)
+  main()
+  
+  # define prior
+  prior <- exp(-5 * abs(p_grid - 0.5))
+  main()
+}
+
+## R code 2.6 - 2.7
+library(rethinking)
