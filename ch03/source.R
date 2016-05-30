05d3eaa3cc3a8b8386a359cabb401b7d61985b1b
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-30 10:56:00 -0500

    more examples

--- ch03/source.R
+++ ch03/source.R
@@ -9,4 +9,13 @@ PrV <- 0.001
 PrP <- PrPV * PrV + PrPM * (1 - PrV)
 print(PrVP <- (PrPV * PrV) / PrP)
 
-## R code 3.2
+## R code 3.2 - 3.5
+p_grid <- seq(from = 0, to = 1, length.out = 1000)
+prior <- rep(1, length(p_grid))
+likelihood <- dbinom(x = 6, size = 9, prob = p_grid)
+posterior <- likelihood * prior
+posterior <- posterior / sum(posterior)
+samples <- sample(p_grid, prob = posterior, size = 10000, replace = TRUE)
+plot(samples)
+library(rethinking)
+dens(samples)
