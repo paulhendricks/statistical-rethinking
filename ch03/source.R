f46544149bc3ff5913e52abad66a764635710541
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-05-31 12:43:27 -0400

    more examples

--- ch03/source.R
+++ ch03/source.R
@@ -20,6 +20,25 @@ plot(samples)
 library(rethinking)
 dens(samples)
 
-## R code 3.2 - 3.5
+## R code 3.6 - 3.19
 # add up posterior probability where p < 0.5
 sum(posterior[p_grid < 0.5])
+sum(samples < 0.5) / 1e4
+sum(0.5 < samples & samples < 0.75) / 1e4
+quantile(samples, 0.8)
+quantile(samples, c(0.1, 0.9))
+p_grid <- seq(from = 0, to = 1, length.out = 1000)
+prior <- rep(1, length(p_grid))
+likelihood <- dbinom(x = 3, size = 3, prob = p_grid)
+posterior <- likelihood * prior
+posterior <- posterior / sum(posterior)
+samples <- sample(p_grid, prob = posterior, size = 10000, replace = TRUE)
+PI(samples, prob = 0.5)
+HPDI(samples, prob = 0.5)
+p_grid[which.max(posterior)]
+chainmode(samples, adj = 0.01)
+mean(samples)
+median(samples)
+sum(posterior * abs(0.5 - p_grid))
+loss <- sapply(p_grid, function(d) sum(posterior * abs(d - p_grid)))
+p_grid[which.min(loss)]
