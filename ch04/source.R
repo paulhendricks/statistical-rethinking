a3a21c0c3896462ecdb9625ca16ab3ec92633f50
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-06-06 14:11:00 -0500

    examples

--- ch04/source.R
+++ ch04/source.R
@@ -17,3 +17,11 @@ small <- replicate(10000, prod(1 + runif(12, 0, 0.01)))
 dens(small, norm.comp = TRUE)
 log.big <- replicate(10000, log(prod(1 + runif(12, 0, 0.5))))
 dens(log.big, norm.comp = TRUE)
+
+## R code 4.6
+w <- 6; n <- 9;
+p_grid <- seq(from = 0, to = 1, length.out = 100)
+posterior <- dbinom(w, n, p_grid) * dunif(p_grid, 0, 1)
+posterior <- posterior / sum(posterior)
+
+## R code 4.7 - 4.
